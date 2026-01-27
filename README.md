011c3fc5a2b1a78bf339aa74e7768ee88e60c283
Hrishikesh <hrishi.halesh.18@gmail.com>
2026-01-27 10:29:35 +0530

    added search and filter functionality in Home Page

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # To-DO.
 
-- search functionality.
-
 - Use dynamic routing to filter books by category (e.g., /books/:category) and Display.
 
-- Genre selector (dropdown in input tag AddBookPage) + (Genre cards in DashBoard) + (Genre filter dropdown in BrowseBooksPage)
+- Genre selector  + (Genre cards in DashBoard)
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import DisplayBooks from './components/DisplayBooks'
 
 function HomePage() {
     const genres = ['Fiction',
@@ -9,19 +11,28 @@ function HomePage() {
         'Comedy',
         'Love',
         'Adventure']
+
+    const [genre, setGenre] = useState('')
+    const [starts_with, setStartsWith] = useState('')
+    const books = useSelector((state) => state.bookManager.totalBooks)
+    let filteredBooks = books;
+
+    if (genre!=='') filteredBooks = books.filter(book => book.genre === genre);
+    if (starts_with!=='') filteredBooks = books.filter(book => book.title?.toLowerCase().includes(starts_with.toLowerCase()));
+
+    const handleClick = (selectedGenre) => {
+        setGenre(selectedGenre)
+    }
     return (
         <div>
             <h1>HomePage</h1>
             {
-                genres.map((genre) =>
-                    <div key={genre} className='p-5 bg-red-100 rounded-xl m-2 inline-block text-amber-700 italic text-3xl font-semibold cursor-pointer shadow-lg hover:bg-amber-200 transition-all duration-300 ease-in-out'
-                    
-                    >
+                genres.map( (genre) =>
+                    <div key={genre} onClick={() => handleClick(genre)} className='p-5 bg-red-100 rounded-xl m-2 inline-block text-amber-700 italic text-3xl font-semibold cursor-pointer shadow-lg hover:bg-amber-200 transition-all duration-300 ease-in-out'>
                         <h2>{genre}</h2>
-                    </div>
-
-                )
+                    </div> )
             }
+            <DisplayBooks genre={genre} starts_with={starts_with} books={filteredBooks}/>
         </div>
     )
 }
